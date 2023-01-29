9becb85437e3f2309329eb2a35d405937ed77d97
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2023-01-29 22:01:56 +0530

    update

--- first.py
+++ first.py
@@ -6,3 +6,10 @@ if 5>2:
 
 
 # this is a comment
+
+#Docstrings
+""""This is muliple line comments""" # use for documentatio purposes
+
+# Escape Characters
+
+print("Thilini is \"final \" year student.")
