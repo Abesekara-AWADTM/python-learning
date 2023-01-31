02406d7136768671915243eaf79b7516ad8fc095
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2023-01-31 14:25:25 +0530

    updated

--- 3.py
+++ 3.py
@@ -1,2 +1,13 @@
 from math import*    # import all math functions
 
+# str function 
+# max function
+
+print(10 +10)
+print(10+10*3)
+print((10+10)*3)
+
+age=25
+print("my age is "+ str(age))
+
+print(max(30,29,8,78))
