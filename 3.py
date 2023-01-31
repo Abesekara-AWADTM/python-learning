58d41c8756899c3417136ec5a24e567e22fb1352
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2023-01-31 14:35:41 +0530

    update

--- 3.py
+++ 3.py
@@ -2,6 +2,10 @@ from math import*    # import all math functions
 
 # str function 
 # max function
+#min function
+#pow function
+#abs function absolute function
+
 
 print(10 +10)
 print(10+10*3)
@@ -10,4 +14,11 @@ print((10+10)*3)
 age=25
 print("my age is "+ str(age))
 
-print(max(30,29,8,78))
+print(max(-30,29,8,-78))
+print(min(-30,29,8,-78, 0.67))
+
+print(5^2) # this is wrong .So use pow function
+print(pow(5,2))
+
+print(-3.0*2)
+print(abs(-3.0*2))
