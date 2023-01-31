0e755cd727458607ae8e7c00a8cbbeabab7bb9b2
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2023-01-31 14:56:15 +0530

    input fun

--- 4.py
+++ 4.py
@@ -3,3 +3,9 @@
 age= input("What is your age? ")
 print(age)
 print("My age is "+ age)
+
+name=input("What is your name? ")
+food=input("What is your fav food? ")
+drink=input("What is your drink? ")
+
+print("My name is "+name+ ", my fav food is "+food+ ' and my fav drink is'+drink)
