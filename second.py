b2b93f0020dc96459c664f16a864e54f025c6eb0
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2023-01-31 13:59:07 +0530

    update

--- second.py
+++ second.py
@@ -1,5 +1,20 @@
+# upper(), lower(), isLower() , replace() , index()
+
+
 Sentence1="I am thilini abesekara"
 print(Sentence1.upper())
 
 Sentence2="I AM SOFTWARE ENGINEER"
 print(Sentence2.lower())
+print(Sentence2.islower())
+
+Sentence3="I AM SOFTWARE ENGINEER"
+print(Sentence3.lower().islower())
+
+Sentence4= "I like burger"
+print(Sentence4.replace("burger", "Pizza"))
+
+print(Sentence4.index("I"))  #  python index start with 0 
+print(Sentence4.index("l")) # calculate spaces also
+
+
