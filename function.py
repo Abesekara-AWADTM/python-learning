a1a0ee45f569f4b82eae42925f1c50fb0289b536
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2023-02-01 18:08:11 +0530

    updated

---
+++ function.py
@@ -0,0 +1,20 @@
+def function(name):
+    print("I Love my " + name)
+
+
+function("puppy")
+
+def per1(name):
+    print(name+"How can you help you")
+
+
+
+# return - end of the function
+
+def calculation(a,b,c):
+    print(a+b+c)
+    print("This is a calculation")
+    return print(a-b-c)
+    print(a-b-+c)
+
+calculation(20,70,10)
